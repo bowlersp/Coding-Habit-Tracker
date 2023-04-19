911f741f080c4230d52f6d6b146c3b513dce9b19
bowlersp <120626722+bowlersp@users.noreply.github.com>
2023-04-18 20:59:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -30,3 +30,20 @@ headers = {
 }
 graph_config_response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 print(graph_config_response.text)
+
+
+#### Post to Graph Program ####
+
+final_graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/graph1"
+
+
+date = input("please enter the date you coded (FORMAT = yyyyMMdd)\n")
+quantity = input("please enter the length of time in hours you coded\n")
+
+date_and_quantity = {
+    "date": date,
+    "quantity": quantity
+}
+
+post_to_graph = requests.post(url=final_graph_endpoint, json=date_and_quantity, headers=headers)
+print(post_to_graph.text)
